9e8bab8f5a8b0f2491752bcad974f5ada13bbade
mohamedalileche <mouhalileche.ma@gmail.com>
2023-04-20 21:28:50 +0200

    Last

---
+++ Portfolio/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer bg-[#171717]">
+        <div className="wrap flex justify-between items-center px-12 text-white font-bold py-6 tl:px-4 tl:py-4 ">
+          <h1 className='logo text-[25px] text-white pb-[1.5rem]'>
+            <strong>Med</strong>Alileche
+          </h1>
+          <div className="flex gap-4 cursor-pointer ">
+            <a href="" target="_blank">
+              <i className="fa-brands fa-twitter border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+            </a>
+            <a href="" target="_blank">
+              <i className="fa-brands fa-instagram border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+            </a>
+            <a href="https://github.com/Mohamedalileche" target="_blank">
+              <i className="fa-brands fa-github border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
